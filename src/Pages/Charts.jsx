7606214fa8d4ece3c65b70e31fa2ef5b7700b879
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-12-03 21:33:37 +0600

    added pie chart

--- src/Pages/Charts.jsx
+++ src/Pages/Charts.jsx
@@ -1,11 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Chart } from "react-google-charts";
+import useAllArticles from "../Hooks/useAllArticles";
+
 
 
 const Charts = () => {
-    return (
-        <div>
-            <h1>pie charts</h1>
-        </div>
-    );
+ const {data:allArticles, isLoading} = useAllArticles();
+ const [allPublisherArticles, setAllPublisherArticles] = useState([]);
+ const [allPublisherName, setAllPublisherName] = useState([]);
+
+
+useEffect(()=>{
+     
+    if(!isLoading) {
+        setAllPublisherArticles(allArticles);
+    }
+    axios.get("https://newspapwer-a-12-server.vercel.app/addPublisher")
+    .then(res => setAllPublisherName(res.data))
+},[allArticles,isLoading])
+
+
+const myArticlesPublish = [["publisherName", "Article"]];
+for (let publish in allPublisherName) {
+    let count = 0;
+     let title = allPublisherName[publish].title.toLowerCase();
+    for (let article in allPublisherArticles) {
+      // console.log(allPublisherArticles[article].publisherName.label.toLowerCase());
+    //   console.log(title);
+       if(allPublisherArticles[article].publisherName.label.toLowerCase() == title) {
+        count = count + 1;
+       }
+    }
+    myArticlesPublish.push([title,count])
+}
+
+// console.log(myArticlesPublish)
+//  const data = [
+//    ["publisherName", "Article"],
+//    ["rokomari", 1],
+//    ["daily bazar", 3],
+//    ["daily star", 0]
+// //    ["Sausage", 10], // Below limit.
+// //    ["Anchovies", 9], // Below limit.
+//  ];
+
+ const options = {
+   title: "Popularity of Articles by Publisher",
+//    sliceVisibilityThreshold: 0.1, // 20%
+ };
+
+  return (
+    <div>
+      <Chart
+        chartType="PieChart"
+        data={myArticlesPublish}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+    </div>
+  );
 };
 
 export default Charts;
